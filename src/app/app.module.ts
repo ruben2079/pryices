fde2d35260d1c986fe240a0ac06a1edf8180d240
Ruben Guzman <pryices@gmail.com>
2023-12-21 14:49:39 -0500

    uploading automatic updated files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* Angular Material - imported libraries */
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+/* End - Angular Material - imported libraries */
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ContainerComponent } from './container/container.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { PollComponent } from './sidenav/poll/poll.component';
+import { NewsletterComponent } from './sidenav/newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidenavComponent,
+    ContainerComponent,
+    MenuComponent,
+    PollComponent,
+    NewsletterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    /* Angular Material */
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
