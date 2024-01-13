0f38b9364b2a094c441fe873968d0d59d99de69a
Ruben Guzman <pryices@gmail.com>
2024-01-13 17:30:15 -0500

    updated file

--- src/app/container/randomproducts/randomproducts.component.ts
+++ src/app/container/randomproducts/randomproducts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RandomproductsService } from 'src/app/service/randomproducts.service';
 import { Randomproducts } from 'src/app/models/randomproducts';
 import { take, takeUntil } from 'rxjs';
+import {Subject} from 'rxjs';
+
 @Component({
   selector: 'app-randomproducts',
   templateUrl: './randomproducts.component.html',
@@ -9,12 +11,20 @@ import { take, takeUntil } from 'rxjs';
 })
 export class RandomproductsComponent implements OnInit {
   randomProductsList: Randomproducts[] = [];
+
+  private ngUnsubscribe = new Subject();
+
   constructor(private randomProductsService: RandomproductsService){
 
   }
 
   ngOnInit(): void {
     this.randomProducts();
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next;
+    this.ngUnsubscribe.complete();
   }
 
   randomProducts(){
